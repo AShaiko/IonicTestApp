8d9f148bc3e1a25ff019d3b7a8a115b91d7486ec
anzhelika.shaiko <anzhelika.shaiko@gengo.online>
2025-04-23 21:27:32 +0300

    Add User entity
    Added user entity. Added database migration. Remove unused controller and class.

--- IonicTestApp/ApplicationContext.cs
+++ IonicTestApp/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using IonicTestApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace IonicTestApp;
@@ -7,4 +8,5 @@ public class ApplicationContext : DbContext
 {
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
 
+    public DbSet<User> Users { get; set; }
 }
--- IonicTestApp/Migrations/20250423182607_AddUsers.Designer.cs
+++ IonicTestApp/Migrations/20250423182607_AddUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IonicTestApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IonicTestApp.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250423182607_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.15");
